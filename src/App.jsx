b8cd11ad1094bc27ef17b9bc2e6f5e8e2ef87426
TulasiRohit107 <tulasirohit1097@gmail.com>
2024-04-26 20:42:33 -0400

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,28 @@ import MediaCard from './components/employeecard.jsx';
 import Data from './data/data.js';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
 
 function App() {
-  const [onLeave, setLeave] = useState(true)
-  console.log(onLeave);
+  const [showOnLeave, setShowOnLeave] = useState(true)
+  console.log(showOnLeave);
 
+  const filteredData = showOnLeave ? Data.filter((employee) => employee.onLeave ) : Data;
+  const toggleOnLeave = (event) => { if(showOnLeave) {setShowOnLeave(false)} else setShowOnLeave(true) };
   return (
     <Container fixed>
-<Button variant="contained">Active</Button>
-<Button variant="outlined">On Leave</Button>
-    <MediaCard data={Data} /> 
+      <Box>
+        <Typography variant="h1" gutterBottom>
+        Leave Dashboard
+      </Typography>
+      </Box>
+      <Stack spacing={2} direction="row">
+        <Switch onChange={toggleOnLeave} />
+</Stack>
+    <MediaCard data={filteredData} /> 
     </Container>
   );
 }
